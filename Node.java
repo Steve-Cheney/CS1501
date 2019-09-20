5c5aebd69a6e0570f6edb9a221649b5c51129982
Stephen Cheney <steve.a.cheney99@gmail.com>
2019-09-20 15:07:27 -0400

    Created Node class
    Each node has a value, a child reference, and a sibling reference associated with it. There are setters and getters for each of those values.

---
+++ Node.java
@@ -0,0 +1,43 @@
+public class Node{
+  private Node sibling_Node;
+  private Node child_Node;
+  private char value;
+
+  public Node(){
+
+  }
+
+  public Node(char value){
+    this(value, null, null);
+  }
+
+  public Node(char v, Node sibling, Node child){
+    this.value = v;
+    this.sibling_Node = sibling;
+    this.child_Node = child;
+  }
+  public void set_Value(char v){
+    this.value = v;
+  }
+
+  public void set_Child(Node child){
+    this.child_Node = child;
+  }
+
+  public void set_Sibling(Node sibling){
+    this.sibling_Node = sibling;
+  }
+
+  public Node get_Child(){
+    return child_Node;
+  }
+  public Node get_Sibling(){
+    return sibling_Node;
+  }
+
+  public char get_Value(){
+    return value;
+  }
+
+
+}
